3de238374c1c10724f8eaed3b0fcd7f36a82cf9a
Antila Hanne <hanne.antila@gmail.com>
2016-01-13 15:36:02 +0200

    header file, first commit

---
+++ mymath.h
@@ -0,0 +1,19 @@
+/*------------------------------------------------------------
+
+HEADER-FILE for mymath.c
+
+
+
+------------------------------------------------------------*/
+
+#ifndef MYMATH_H
+#define MYMATH_H
+#define PI  3.14159265358979323846264338328
+extern double dmax(double, double);
+extern double dmin(double, double);
+extern double dround(double);
+extern double SQR(double);
+extern double Exponential_Integral_Ei( double x );
+
+
+#endif
